8c7cb0645bb35e3875ea67e778471bad2d716615
jefferygilbert <33426666+jefferygilbert@users.noreply.github.com>
2019-03-24 21:26:10 -0400

    Updates

---
+++ precache-manifest.97f29b42b14415614ad04e25b166ba23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "27ea407bb12805fd9c08",
+    "url": "/robofriends/static/js/main.27ea407b.chunk.js"
+  },
+  {
+    "revision": "8aee7835c63231cd44be",
+    "url": "/robofriends/static/js/1.8aee7835.chunk.js"
+  },
+  {
+    "revision": "27ea407bb12805fd9c08",
+    "url": "/robofriends/static/css/main.545bad7d.chunk.css"
+  },
+  {
+    "revision": "8aee7835c63231cd44be",
+    "url": "/robofriends/static/css/1.ce4d194b.chunk.css"
+  },
+  {
+    "revision": "85a8b7974fa8bc168895e1bfae48a637",
+    "url": "/robofriends/index.html"
+  }
+];
